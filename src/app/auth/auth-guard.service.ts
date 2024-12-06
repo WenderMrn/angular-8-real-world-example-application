c9ec6420ed140d56c90144a0182d5e97fc5caf7a
Mariano Laurentino <wrmo@gft.com>
2024-12-06 17:34:51 -0300

    feat: Angular 16

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, CanActivateChild } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { JwtService } from '../shared';
 import { AuthService } from './auth.services';
 import { Observable, of } from 'rxjs';
 import { take } from 'rxjs/operators';
 
 @Injectable()
-export class AuthGuardService implements CanActivate, CanActivateChild {
+export class AuthGuardService  {
     constructor(
         private router: Router,
         private jwtService: JwtService,
